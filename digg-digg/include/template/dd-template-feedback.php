008fbe2d69842070bae676bfb1b38f3fb694c5c6
Andrew Yates <andrew@383project.com>
2012-05-14 16:50:52 +0100

    Submit form to diggdigg@bufferapp.com - Email needs tweaking to tidy it up. - Wording within form needs tweaking to be more friendly :)

--- digg-digg/include/template/dd-template-feedback.php
+++ digg-digg/include/template/dd-template-feedback.php
@@ -1,12 +1,37 @@
 <?php
 
 function dd_feedback_setup(){
+	$dd_feedback_errors = array();
+	
+	if($_POST['submit']) {
+		$dd_feedback_name = $_POST['object']['name'];
+		$dd_feedback_email = $_POST['object']['email'];
+		$dd_feedback_message = $_POST['object']['message'];
+		$dd_feedback_diagnostics = $_POST['object']['diagnostics'];
+	
+		if($dd_feedback_name=='') $dd_feedback_errors[] = 'Please make sure you have entered a name.';
+		if($dd_feedback_email=='') $dd_feedback_errors[] = 'Please make sure you have entered a valid email address.';
+		//if(!array_key_exists($this->data->type, $this->feedback_types)) $dd_feedback_errors[] = 'Please make sure you have selected a feedback type.';
+		if($dd_feedback_message == '') $dd_feedback_errors[] = 'Please make sure you have entered a message.';
+		if(strlen($dd_feedback_message)>2500) $dd_feedback_errors[] = 'Please make sure your message is less than 2500 characters in length.';
+		
+		if(count($dd_feedback_errors)==0) {
+			// Send email to diggdigg@bufferapp.com
+			$dd_feedback_subject = "Digg Digg Feedback ". DD_VERSION;
+			$dd_feedback_message = "Name: $dd_feedback_name.\n\n Email: $dd_feedback_email.\n\n Message: $dd_feedback_message\n\n Diagnostics: $dd_feedback_diagnostics";
+			wp_mail("diggdigg@bufferapp.com", $dd_feedback_subject, $dd_feedback_message); 
+			
+			dd_feedback_success();
+			return;
+		}
+	}
+
 	$diagnostics = dd_get_diagnostics();
 ?>
 
 	<div class="wrap">
 				
-		<?php if($result['status']=='error'): ?><div class="error"><ul style="padding-top:6px;"><li><?php echo implode('</li><li>',$result['errors']); ?></li></ul></div><?php endif; ?>
+		<?php if(count($dd_feedback_errors) != 0): ?><div class="error"><ul style="padding-top:6px;"><li><?php echo implode('</li><li>',$dd_feedback_errors); ?></li></ul></div><?php endif; ?>
 		
 		<h2>Digg Digg <?php echo DD_VERSION; ?> Feedback</h2>
 		<p>Welcome to the feedback page for Digg Digg. Here you can quickly and easily send messages to the plugin developers. If you are having trouble getting something to work, would like to know how to perform a certain action or have a new feature request just use the form below to get in touch.</p>
@@ -19,12 +44,12 @@ function dd_feedback_setup(){
 				</tr>
 				<tr>
 					<th scope="row">Your name</th>
-					<td><input type="text" name="object[name]" value="" class="regular-text" /></td>
+					<td><input type="text" name="object[name]" value="<?php echo dd_e($dd_feedback_name); ?>" class="regular-text" /></td>
 				</tr>
 				<tr>
 					<th scope="row">Your email address</th>
 					<td>
-						<input type="text" name="object[email]" value="" class="regular-text" />
+						<input type="text" name="object[email]" value="<?php echo dd_e($dd_feedback_email); ?>" class="regular-text" />
 						<br /><span class="description">This will only be used to reply to you</span>
 					</td>
 				</tr>
@@ -42,7 +67,7 @@ function dd_feedback_setup(){
 				<tr>
 					<th scope="row">Your message<br /><span class="description">Max 2500 chars</span></th>
 					<td>
-						<textarea name="object[message]" class="large-text" rows="10" cols="50"></textarea>
+						<textarea name="object[message]" class="large-text" rows="10" cols="50"><?php echo dd_e($dd_feedback_message); ?></textarea>
 					</td>
 				</tr>
 				<?php if(is_array($diagnostics) and count($diagnostics)>0): ?>
@@ -50,7 +75,7 @@ function dd_feedback_setup(){
 						<th scope="row">Diagnostic data</th>
 						<td>
 							The following information will also be sent with your message, it allows us to more easily identify any potential problems. No personally identifiable data will be transmitted except for what you enter above.
-							<textarea name="object[message]" class="large-text" rows="5" cols="50" disabled="disabled"><?php foreach($diagnostics as $k=>$v): ?><?php echo $k.': '.$v."\n"; ?><?php endforeach; ?></textarea>
+							<textarea name="object[diagnostics]" class="large-text" rows="5" cols="50"><?php foreach($diagnostics as $k=>$v): ?><?php echo $k.': '.$v."\n"; ?><?php endforeach; ?></textarea>
 						</td>
 					</tr>
 				<?php endif; ?>
@@ -64,7 +89,32 @@ function dd_feedback_setup(){
 	</div>
 
 <?php
-}
+} // end func: dd_feedback_setup
+
+
+
+function dd_feedback_success(){
+?>
+	<div class="wrap">
+		<h2>Digg Digg <?php echo DD_VERSION; ?> Feedback</h2>
+		<div class="updated"><p>Feedback sent successfully.</p></div>
+		<p>Thank you for getting in touch. Your message has been sent to the plugin developer(s).</p>
+	</div>
+
+<?php
+} // end func: dd_feedback_success
+
+
+
+
+
+/*
+	Sanitise output
+*/
+function dd_e($str) {
+	return htmlspecialchars($str);
+} // end func: dd_e
+
 
 
 function dd_get_diagnostics() {
