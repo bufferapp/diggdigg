207736dd45cbf46c07783f3474cb318a70ed06cb
Andrew Yates <contact@andydev.co.uk>
2013-05-04 18:07:20 +0100

    Add ability to override starting element site wide, custom field override is checked before falling back to site wide and then #dd_start if none found.

--- digg-digg/include/template/dd-template-floating-display.php
+++ digg-digg/include/template/dd-template-floating-display.php
@@ -222,6 +222,14 @@ function checkCategory(){
 						        		<input name=<?php echo DD_COMMENT_ANCHOR_OPTION; ?> type="checkbox" <?php echo ($ddFloatDisplay[DD_COMMENT_ANCHOR_OPTION][DD_COMMENT_ANCHOR_OPTION_STATUS]==DD_DISPLAY_ON) ? DD_CHECK_BOX_ON : DD_CHECK_BOX_OFF ?>>
 						        	</td>
 						        </tr>
+						        
+						        <tr valign="top">
+						        	<th scope="row">2.7 Override initial floating bar location</th>
+						        	<td>
+										Element : #<input type="text" value="<?php echo $ddFloatDisplay[DD_FLOAT_OPTION][DD_FLOAT_OPTION_INITIAL_ELEMENT] ?>" 
+											name="<?php echo DD_FLOAT_OPTION_INITIAL_ELEMENT;?>" /> (without preceding #)
+						        	</td>
+						        </tr>
 						    </table>
 							
 							<div class="submit">
